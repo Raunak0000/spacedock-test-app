484dffed241ac2973a347e92031433797295b536
Raunak Warrier <raunakwarrier@outlook.com>
2026-03-26 11:44:07 +0530

    fixed dockerfile

---
+++ Main.java
@@ -0,0 +1,20 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "Hello from SpaceDock! Deployment is working!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.start();
+        System.out.println("Test app running on port 8080");
+    }
+}
